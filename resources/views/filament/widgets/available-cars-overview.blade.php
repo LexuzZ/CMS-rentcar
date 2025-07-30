d56be2fe9c674e4a2b6bb0fb0d52445a5d9800b2
Haerul Taka <haerultaka255@gmail.com>
2025-07-31 00:10:45 +0800

    adding card car ready dashboard

--- resources/views/filament/widgets/available-cars-overview.blade.php
+++ resources/views/filament/widgets/available-cars-overview.blade.php
@@ -1,7 +1,7 @@
 <x-filament::widget>
     <x-filament::card>
-        <div class="p-6">
-            <h2 class="text-2xl font-bold pb-6 text-gray-800">Mobil Tersedia (Ready)</h2>
+        <div class="p-2">
+            <h2 class="text-md font-bold pb-6 text-gray-800">Mobil Tersedia (Ready)</h2>
 
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
                 @forelse ($cars as $car)
@@ -10,7 +10,7 @@
                             <x-heroicon-o-truck class="w-6 h-6" />
                         </div>
                         <div>
-                            <div class="text-lg font-semibold text-gray-900 dark:text-white">{{ $car->nama_mobil }}</div>
+                            <div class="text-sm font-semibold text-gray-900 dark:text-white">{{ $car->nama_mobil }}</div>
                             <div class="text-sm text-gray-500 dark:text-gray-300">{{ $car->total }} Unit</div>
                         </div>
                     </div>
