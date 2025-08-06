f105a79077c01bc3af06ebcf88ab69d721e032b3
Haerul Taka <haerultaka255@gmail.com>
2025-08-06 19:34:09 +0800

    edit pesanan

--- app/Filament/Resources/BookingResource.php
+++ app/Filament/Resources/BookingResource.php
@@ -63,48 +63,6 @@ class BookingResource extends Resource
                 TextInput::make('id')
                     ->hidden()
                     ->dehydrated(), // agar nilainya ikut dikirim ke validasi
-
-                Select::make('car_id')
-                    ->label('Mobil (No Polisi)')
-                    ->relationship('car', 'nopol')
-                    ->searchable()
-                    ->preload()
-                    ->required()
-                    ->reactive()
-                    ->afterStateUpdated(function ($state, callable $set, callable $get) {
-                        if ($state && $car = \App\Models\Car::find($state)) {
-                            $set('harga_harian', $car->harga_harian);
-                        } else {
-                            $set('harga_harian', 0);
-                        }
-
-                        static::calculatePrice($set, $get); // ✅ sudah dikirim lengkap
-                    }),
-                Select::make('customer_id')
-                    ->label('Pelanggan')
-                    ->relationship('customer', 'nama')
-                    ->searchable()
-                    ->preload()
-                    ->required(),
-
-                Select::make('driver_id')
-                    ->label('Sopir (Opsional)')
-                    ->relationship('driver', 'nama')
-                    ->searchable()
-                    ->preload()
-                    ->nullable(),
-
-                Select::make('paket')
-                    ->label('Paket Sewa')
-                    ->options([
-                        'lepas_kunci' => 'Lepas Kunci',
-                        'dengan_driver' => 'Dengan Driver',
-                        'tour' => 'Paket Tour',
-                    ])
-                    ->nullable(),
-
-
-
                 DatePicker::make('tanggal_keluar')
                     ->label('Tanggal Keluar')
                     ->required()
@@ -154,6 +112,67 @@ class BookingResource extends Resource
                     ->label('Waktu Kembali')
                     ->nullable()
                     ->seconds(false),
+
+                Select::make('car_id')
+                    ->label('Mobil (No Polisi)')
+                    ->relationship('car', 'nopol')
+                    ->searchable()
+                    ->preload()
+                    ->required()
+                    ->reactive()
+                    ->afterStateUpdated(function ($state, callable $set, callable $get) {
+                        if ($state && $car = \App\Models\Car::find($state)) {
+                            $set('harga_harian', $car->harga_harian);
+                        } else {
+                            $set('harga_harian', 0);
+                        }
+
+                        static::calculatePrice($set, $get); // ✅ sudah dikirim lengkap
+                    }),
+                Select::make('customer_id')
+                    ->label('Pelanggan')
+                    ->relationship('customer', 'nama')
+                    ->searchable()
+                    ->preload()
+                    ->createOptionForm([
+                        Forms\Components\TextInput::make('nama')
+                            ->label('Nama Pelanggan')
+                            ->required(),
+                        Forms\Components\TextInput::make('no_telp')
+                            ->label('No. HP')
+                            ->tel()
+                            ->required()
+                            ->unique(ignoreRecord: true),
+                        Forms\Components\TextInput::make('alamat')
+                            ->label('Alamat')
+                            ->required(),
+                            
+                        TextInput::make('ktp')
+                            ->label('No KTP')
+                            ->required()
+                            ->unique(ignoreRecord: true),
+                    ])
+                    ->required(),
+
+                Select::make('driver_id')
+                    ->label('Sopir (Opsional)')
+                    ->relationship('driver', 'nama')
+                    ->searchable()
+                    ->preload()
+                    ->nullable(),
+
+                Select::make('paket')
+                    ->label('Paket Sewa')
+                    ->options([
+                        'lepas_kunci' => 'Lepas Kunci',
+                        'dengan_driver' => 'Dengan Driver',
+                        'tour' => 'Paket Tour',
+                    ])
+                    ->nullable(),
+
+
+
+
                 // TextInput::make('estimasi_biaya')
                 //     ->label('Harga Sewa')
                 //     ->numeric()
@@ -212,34 +231,6 @@ class BookingResource extends Resource
     {
         return $table
             ->columns([
-                TextColumn::make('car.nopol')->label('No Polisi')->alignCenter(),
-                TextColumn::make('invoice.booking.car.merek')
-                    ->label('Merk Mobil')
-                    ->badge()
-                    ->searchable()
-                    ->alignCenter()
-                    ->colors([
-                        'info' => 'toyota',
-                        'info' => 'mitsubishi',
-                        'info' => 'suzuki',
-                        'info' => 'daihatsu',
-                        'info' => 'honda'
-                    ])
-                    ->formatStateUsing(fn($state) => match ($state) {
-                        'toyota' => 'Toyota',
-                        'mitsubishi' => 'Mitsubishi',
-                        'suzuki' => 'Suzuki',
-                        'honda' => 'Honda',
-                        'daihatsu' => 'Daihatsu',
-                        default => ucfirst($state),
-                    }),
-                TextColumn::make('customer.nama')->label('Pelanggan')->alignCenter()->searchable(),
-                // TextColumn::make('driver.nama')->label('Sopir')->toggleable(),
-                TextColumn::make('tanggal_keluar')->label('Tanggal Keluar')->date('d M Y')->alignCenter(),
-                TextColumn::make('tanggal_kembali')->label('Tanggal Kembali')->date('d M Y')->alignCenter(),
-                TextColumn::make('waktu_keluar')->label('Waktu Keluar')->alignCenter()->time('H:i'),
-                TextColumn::make('waktu_kembali')->label('Waktu Kembali')->alignCenter()->time('H:i'),
-                TextColumn::make('estimasi_biaya')->label('Biaya')->money('IDR')->alignCenter(),
                 TextColumn::make('status')
                     ->label('Status')
                     ->badge()
@@ -257,16 +248,45 @@ class BookingResource extends Resource
                         'batal' => 'Batal',
                         default => ucfirst($state),
                     }),
+                TextColumn::make('car.nopol')->label('No Polisi')->alignCenter(),
+                TextColumn::make('car.nama_mobil')->label('Type Mobil')->alignCenter(),
+                // TextColumn::make('invoice.booking.car.merek')
+                //     ->label('Merk Mobil')
+                //     ->badge()
+                //      ->searchable()
+                //     ->alignCenter()
+                //     ->colors([
+                //         'info' => 'toyota',
+                //         'info' => 'mitsubishi',
+                //         'info' => 'suzuki',
+                //         'info' => 'daihatsu',
+                //         'info' => 'honda'
+                //     ])
+                //     ->formatStateUsing(fn($state) => match ($state) {
+                //         'toyota' => 'Toyota',
+                //         'mitsubishi' => 'Mitsubishi',
+                //         'suzuki' => 'Suzuki',
+                //         'honda' => 'Honda',
+                //         'daihatsu' => 'Daihatsu',
+                //         default => ucfirst($state),
+                //     }),
+
+                TextColumn::make('customer.nama')->label('Pelanggan')->alignCenter()->searchable(),
+                // TextColumn::make('driver.nama')->label('Sopir')->toggleable(),
+                TextColumn::make('tanggal_keluar')->label('Tanggal Keluar')->date('d M Y')->alignCenter(),
+                TextColumn::make('tanggal_kembali')->label('Tanggal Kembali')->date('d M Y')->alignCenter(),
+                TextColumn::make('waktu_keluar')->label('Waktu Keluar')->alignCenter()->time('H:i'),
+                TextColumn::make('waktu_kembali')->label('Waktu Kembali')->alignCenter()->time('H:i'),
+                TextColumn::make('estimasi_biaya')->label('Biaya')->money('IDR')->alignCenter(),
+
 
             ])
             ->filters([
                 SelectFilter::make('status')
-                    ->label('Status ')
+                    ->label('Status Pembayaran')
                     ->options([
-                        'aktif' => 'Aktif',
-                        'booking' => 'Booking',
-                        'selesai' => 'Selesai',
-                        'batal' => 'Batal',
+                        'lunas' => 'Lunas',
+                        'belum_lunas' => 'Belum Lunas',
 
                     ]),
             ])
