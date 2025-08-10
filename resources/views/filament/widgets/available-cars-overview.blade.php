9a26a3ef4d827de9e84a4fd0780d334506262cdf
Haerul Taka <haerultaka255@gmail.com>
2025-08-10 13:42:03 +0800

    update car list

--- resources/views/filament/widgets/available-cars-overview.blade.php
+++ resources/views/filament/widgets/available-cars-overview.blade.php
@@ -1,5 +1,3 @@
-{{-- File: resources/views/filament/widgets/nama-widget-anda.blade.php --}}
-
 <x-filament::widget>
     <x-filament::card>
         <h2 class="text-xs font-bold mb-4">Mobil Tersedia</h2>
@@ -15,9 +13,9 @@
                 <select class="custom-select w-full bg-gray-800 dark:text-gray-800 text-xs rounded-md">
                     <option value="" class="text-gray-800">Pilih mobil {{ Illuminate\Support\Str::title($merek) }}</option>
                     @foreach ($mobilList as $mobil)
-                        {{-- SEKARANG KITA MASUKKAN HTML DI SINI --}}
                         <option value="{{ $mobil->id }}">
-                            {{ $mobil->nama_mobil }}
+                            {{-- PERUBAHAN DI SINI: Mengambil nama model dari relasi --}}
+                            {{ $mobil->carModel->name }}
                             <span class='bg-gray-200 text-gray-800 text-xs font-medium ms-2 px-2 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300'>
                                 {{$mobil->nopol}}
                             </span>
@@ -29,4 +27,3 @@
         </div>
     </x-filament::card>
 </x-filament::widget>
-
