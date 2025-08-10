7dedecfb4d1739f0c2b32cbbd19faeea8956a423
Haerul Taka <haerultaka255@gmail.com>
2025-08-10 13:34:30 +0800

    update tabel brandcarmodel

--- database/seeders/BrandAndModelSeeder.php
+++ database/seeders/BrandAndModelSeeder.php
@@ -10,21 +10,36 @@ class BrandAndModelSeeder extends Seeder
 {
     public function run(): void
     {
+        // Daftar lengkap merek dan model mobil
         $data = [
-            'Toyota' => ['Avanza', 'Innova Reborn','Innova Zenix', 'Rush', 'Fortuner', 'Calya'],
-            'Mitsubishi' => ['Xpander', 'Pajero Sport', 'Triton'],
-            'Suzuki' => ['Ertiga', 'XL7', 'Carry'],
-            'Daihatsu' => ['Xenia', 'Terios', 'Sigra', 'Gran Max'],
-            'Honda' => ['Brio', 'HR-V', 'CR-V', 'Mobilio'],
+            'Toyota' => [
+                'Innova Zenix G HV', 'Innova Zenix G', 'Innova Zenix Q', 'New Agya', 'Raize',
+                'Alphard', 'Camry', 'Yaris', 'Corolla', 'Vellfire', 'Voxy',
+                'Land Cruiser', 'Hilux 4x4', 'Hiace', 'Hiace Premio'
+            ],
+            'Daihatsu' => [
+                'Ayla', 'Xenia', 'Sigra', 'Terios', 'Rocky', 'Sirion', 'Gran Max'
+            ],
+            'Suzuki' => [
+                'Ertiga', 'XL7', 'Jimny', 'Baleno', 'Pickup', 'Blindvan'
+            ],
+            'Honda' => [
+                'Brio', 'Jazz', 'Mobilio', 'HR-V', 'CR-V', 'City', 'Civic'
+            ],
+            'Mitsubishi' => [
+                'Xpander', 'Pajero Sport', 'L300'
+            ],
         ];
 
+        // Loop untuk membuat data
         foreach ($data as $brandName => $models) {
-            // Buat merek baru
-            $brand = Brand::create(['name' => $brandName]);
+            // Buat atau temukan merek
+            $brand = Brand::firstOrCreate(['name' => $brandName]);
 
             // Buat model-model yang berelasi dengan merek tersebut
             foreach ($models as $modelName) {
-                $brand->carModels()->create(['name' => $modelName]);
+                // firstOrCreate akan mencegah duplikasi jika seeder dijalankan lagi
+                $brand->carModels()->firstOrCreate(['name' => $modelName]);
             }
         }
     }
