088715ebecb01afad70d027f84f253e17af88ab5
Haerul Taka <haerultaka255@gmail.com>
2025-08-13 22:25:17 +0800

    add sop

---
+++ resources/views/filament/widgets/announcement-widget.blade.php
@@ -0,0 +1,36 @@
+<x-filament-widgets::widget>
+    <x-filament::section>
+        <div class="space-y-4">
+            @foreach ($announcements as $announcement)
+                @php
+                    // Logika untuk menentukan warna berdasarkan tipe
+                    $colors = match ($announcement['type']) {
+                        'warning' => 'bg-yellow-50 text-yellow-800 dark:bg-yellow-800/20 dark:text-yellow-400',
+                        'danger' => 'bg-red-50 text-red-800 dark:bg-red-800/20 dark:text-red-400',
+                        'success' => 'bg-green-50 text-green-800 dark:bg-green-800/20 dark:text-green-400',
+                        default => 'bg-blue-50 text-blue-800 dark:bg-blue-800/20 dark:text-blue-400',
+                    };
+                    $icon = match ($announcement['type']) {
+                        'warning' => 'heroicon-o-exclamation-triangle',
+                        'danger' => 'heroicon-o-x-circle',
+                        'success' => 'heroicon-o-check-circle',
+                        default => 'heroicon-o-information-circle',
+                    };
+                @endphp
+                <div class="p-4 rounded-lg {{ $colors }}">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <x-dynamic-component :component="$icon" class="h-5 w-5" />
+                        </div>
+                        <div class="ml-3">
+                            <h3 class="text-sm font-medium">{{ $announcement['title'] }}</h3>
+                            <div class="mt-2 text-sm">
+                                <p>{{ $announcement['content'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </x-filament::section>
+</x-filament-widgets::widget>
