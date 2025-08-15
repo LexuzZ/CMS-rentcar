016fdb7415daf0d958d79cbd5a4cfc709f5ad820
Haerul Taka <haerultaka255@gmail.com>
2025-08-15 15:11:43 +0800

    edit layout dashboard

--- resources/views/filament/widgets/staff-ranking-widget.blade.php
+++ resources/views/filament/widgets/staff-ranking-widget.blade.php
@@ -13,7 +13,7 @@
 
         {{-- Tabel Peringkat Staff --}}
         <div class="overflow-x-auto">
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <table class="w-full text-xs text-left text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="px-4 py-3">Rank</th>
