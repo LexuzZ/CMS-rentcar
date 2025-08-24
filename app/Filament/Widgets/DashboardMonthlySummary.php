d7aa4534254622b34a62bb0ac4ba38e823570073
Haerul Taka <haerultaka255@gmail.com>
2025-08-25 07:33:38 +0800

    add stat in dashboard mobil keluar dan kembali

--- app/Filament/Widgets/DashboardMonthlySummary.php
+++ app/Filament/Widgets/DashboardMonthlySummary.php
@@ -13,6 +13,7 @@ use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class DashboardMonthlySummary extends BaseWidget
 {
+    protected int|string|array $columnSpan = 'full';
 
    protected function getStats(): array
     {
