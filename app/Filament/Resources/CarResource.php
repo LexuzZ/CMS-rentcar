ed0a9cd9e72e2e94a96f9c4d6ebc952066ad0cf5
Haerul Taka <haerultaka255@gmail.com>
2025-09-07 23:12:16 +0800

    update list mobil

--- app/Filament/Resources/CarResource.php
+++ app/Filament/Resources/CarResource.php
@@ -227,7 +227,7 @@ class CarResource extends Resource
 
                         $textToCopy = "Halo,✋ Lombok 😊\nMobil yang tersedia di Garasi Semeton Pesiar periode *{$startDateTime}* sampai *{$endDateTime}* :\n\n";
                         foreach ($cars as $index => $car) {
-                            $textToCopy .= ($index + 1) . ".🚗 *{$car->carModel->brand->name} {$car->carModel->name}* - {$car->nopol}\n";
+                            $textToCopy .= ($index + 1) . ".🚗 *{$car->carModel->name}*-{($car->nopol)}\n";
                         }
                         $textToCopy .= "\nInfo lebih lanjut bisa hubungi kami. Terima kasih.\n\n📞  WA: 081907367197\n🌐  Website: www.semetonpesiar.com";
 
