0f1e13d380256df01059c806cf531c1facc197bf
Haerul Taka <haerultaka255@gmail.com>
2025-09-11 21:32:09 +0800

    update fitur checklist

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -49,11 +49,19 @@ class AdminPanelProvider extends PanelProvider
             //   ->brandLogo(asset('semetonpesiar.svg'))
             // ->domain('');
             ->colors([
-                'primary' => Color::Blue,
-                'success' => Color::Emerald,
-                'danger' => Color::Red,
-                'info' => Color::Sky,
-                'warning' => Color::Amber,
+                'primary' => [ // <-- Definisikan palet kustom di sini
+                    50 => '239, 249, 255',
+                    100 => '224, 242, 254',
+                    200 => '186, 230, 253',
+                    300 => '125, 211, 252',
+                    400 => '56, 189, 248',
+                    500 => '14, 116, 144', // Warna biru laut sebagai dasar
+                    600 => '8, 145, 178',
+                    700 => '14, 116, 144',
+                    800 => '21, 94, 117',
+                    900 => '22, 78, 99',
+                    950 => '8, 51, 68',
+                ],
                 'gray' => Color::Slate,
             ])
 
