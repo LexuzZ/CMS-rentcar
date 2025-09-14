47dc5d81b5914849098823bf8c12367c783f5797
Haerul Taka <haerultaka255@gmail.com>
2025-09-14 21:05:01 +0800

    add stat booking

--- app/Filament/Resources/BookingResource/Pages/ListBookings.php
+++ app/Filament/Resources/BookingResource/Pages/ListBookings.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\BookingResource\Pages;
 
 use App\Filament\Resources\BookingResource;
+use App\Filament\Resources\BookingResource\Widgets\BookingStats;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
@@ -18,6 +19,12 @@ class ListBookings extends ListRecords
                 ->icon('heroicon-o-plus')
                 ->color('success')
 
+        ];
+    }
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            BookingStats::class,
         ];
     }
 }
