1465ada3e756161f582b4c249afd2d7c023de107
Haerul Taka <haerultaka255@gmail.com>
2025-09-14 21:10:35 +0800

    add stat booking

--- app/Filament/Resources/BookingResource/Widgets/BookingStats.php
+++ app/Filament/Resources/BookingResource/Widgets/BookingStats.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\BookingResource\Widgets;
 
 use App\Models\Booking;
+use Carbon\Carbon;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -10,16 +11,31 @@ class BookingStats extends BaseWidget
 {
     protected function getStats(): array
     {
+        $now = Carbon::now();
         return [
-            Stat::make('Sedang Booking', Booking::where('status', 'booking')->count())
+            Stat::make('Total Booking Bulan Ini', Booking::whereMonth('tanggal_keluar', $now->month)
+                ->whereYear('tanggal_keluar', $now->year)
+                ->count())
+                ->description('Semua booking bulan ini')
+                ->color('primary'),
+            Stat::make('Sedang Booking', Booking::where('status', 'booking')
+                ->whereMonth('tanggal_keluar', $now->month)
+                ->whereYear('tanggal_keluar', $now->year)
+                ->count())
                 ->description('Belum diproses')
                 ->color('info'),
 
-            Stat::make('Disewa', Booking::where('status', 'disewa')->count())
+            Stat::make('Disewa', Booking::where('status', 'disewa')
+                ->whereMonth('tanggal_keluar', $now->month)
+                ->whereYear('tanggal_keluar', $now->year)
+                ->count())
                 ->description('Unit sedang jalan')
                 ->color('warning'),
 
-            Stat::make('Selesai', Booking::where('status', 'selesai')->count())
+            Stat::make('Selesai', Booking::where('status', 'selesai')
+                ->whereMonth('tanggal_keluar', $now->month)
+                ->whereYear('tanggal_keluar', $now->year)
+                ->count())
                 ->description('Transaksi selesai')
                 ->color('success'),
         ];
