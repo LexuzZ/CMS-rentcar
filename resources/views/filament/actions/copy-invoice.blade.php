b87beeb941062d61332a9945434e88fdd82848f2
Haerul Taka <haerultaka255@gmail.com>
2025-09-15 12:44:31 +0800

    update layout filter booking

--- resources/views/filament/actions/copy-invoice.blade.php
+++ resources/views/filament/actions/copy-invoice.blade.php
@@ -1,7 +1,9 @@
 <div>
-    <textarea id="invoice-text" class="w-full text-sm border rounded p-2 text-black dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200" rows="12" readonly>{{ $textToCopy }}</textarea>
+    <textarea id="invoice-text"
+        class="w-full text-sm border rounded p-2 text-black dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200"
+        rows="12" readonly>{{ $textToCopy }}</textarea>
 
-    <div class="mt-4 flex justify-end">
+    {{-- <div class="mt-4 flex justify-end">
         <x-filament::button
             color="primary"
             icon="heroicon-o-clipboard"
@@ -15,5 +17,35 @@
         >
             Copy ke Clipboard
         </x-filament::button>
+
+    </div> --}}
+    <div class="mt-4 flex justify-end" x-data="{
+        buttonText: 'Copy ke Clipboard',
+        async copyToClipboard() {
+            const text = document.getElementById('text-to-copy-area').value;
+
+            try {
+                await navigator.clipboard.writeText(text);
+                this.buttonText = 'Berhasil Disalin! ✅';
+
+                setTimeout(() => {
+                    this.buttonText = 'Copy ke Clipboard';
+                }, 2000);
+            } catch (err) {
+                this.buttonText = 'Gagal Menyalin ❌';
+                setTimeout(() => {
+                    this.buttonText = 'Copy ke Clipboard';
+                }, 2000);
+            }
+        }
+    }">
+        <button x-on:click="copyToClipboard" type="button" class="fi-btn fi-btn-size-md fi-btn-color-primary">
+            <span x-text="buttonText"></span>
+        </button>
     </div>
+
+    <textarea id="text-to-copy-area" class="hidden">
+Halo, ini contoh teks faktur yang akan disalin ke clipboard.
+</textarea>
+
 </div>
