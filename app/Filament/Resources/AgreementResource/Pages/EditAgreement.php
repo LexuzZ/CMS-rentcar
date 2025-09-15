6333a8ec841e757b1e5357cd38b8477cabf6098a
Haerul Taka <haerultaka255@gmail.com>
2025-09-15 21:00:35 +0800

    add foto bbm checklist

--- app/Filament/Resources/AgreementResource/Pages/EditAgreement.php
+++ app/Filament/Resources/AgreementResource/Pages/EditAgreement.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\AgreementResource\Pages;
 use App\Filament\Resources\AgreementResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class EditAgreement extends EditRecord
 {
@@ -13,7 +14,45 @@ class EditAgreement extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            // Actions\DeleteAction::make(),
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getFormActions(): array
+    {
+        return [
+            Actions\Action::make('downloadPdf')
+                ->label('Cetak PDF')
+                ->icon('heroicon-o-arrow-down-tray')
+                ->color('info')
+                ->button()
+                ->action(function () {
+                    // Ambil data dari formulir, termasuk base64 foto BBM
+                    $data = $this->form->getState();
+                    $fotoBbmData = $data['foto_bbm'] ?? null;
+
+                    // Buat PDF dengan data booking dan foto BBM
+                    $pdf = Pdf::loadView('pdf.agreement', [
+                        'booking' => $this->getRecord(),
+                        'foto_bbm' => $fotoBbmData,
+                    ]);
+
+                    return response()->streamDownload(
+                        fn () => print($pdf->output()),
+                        "Perjanjian-Booking-{$this->getRecord()->customer->nama}.pdf"
+                    );
+                }),
+            Actions\Action::make('simpan')
+                ->label('Simpan')
+                ->icon('heroicon-o-check')
+                ->color('success')
+                ->submit('save'),
+            Actions\Action::make('cancel')
+                ->label('Batal')
+                ->icon('heroicon-o-x-mark')
+                ->color('gray')
+                ->url(static::getResource()::getUrl('index')),
         ];
     }
 }
