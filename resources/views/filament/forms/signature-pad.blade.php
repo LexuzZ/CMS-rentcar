fdcf91df441c107a79f6ef9fc910851560b680c4
Haerul Taka <haerultaka255@gmail.com>
2025-09-15 13:16:01 +0800

    add foto bbm checklist

--- resources/views/filament/forms/signature-pad.blade.php
+++ resources/views/filament/forms/signature-pad.blade.php
@@ -1,11 +1,8 @@
 @php
     $statePath = $getStatePath();
 @endphp
-
 <div x-data="signaturePad({ state: $wire.entangle('{{ $statePath }}') })">
-    <canvas x-ref="canvas"
-        class="border border-gray-300 rounded-lg shadow-sm w-full"
-        style="height: 250px;"></canvas>
+    <canvas x-ref="canvas" class="border border-gray-300 rounded-lg shadow-sm w-full" style="height: 250px;"></canvas>
 
     <div class="flex gap-2 mt-2">
         <x-filament::button color="danger" size="sm" @click="clear">Hapus</x-filament::button>
@@ -15,34 +12,21 @@
 <script src="https://cdn.jsdelivr.net/npm/signature_pad@4.1.7/dist/signature_pad.umd.min.js"></script>
 <script>
     document.addEventListener('alpine:init', () => {
-        Alpine.data('signaturePad', ({ state }) => ({
+        Alpine.data('signaturePad', ({
+            state
+        }) => ({
             signaturePadInstance: null,
             state: state,
 
             init() {
                 this.$nextTick(() => {
                     const canvas = this.$refs.canvas;
+                    const ratio = Math.max(window.devicePixelRatio || 1, 1);
+                    canvas.width = canvas.offsetWidth * ratio;
+                    canvas.height = 250 * ratio;
+                    canvas.getContext("2d").scale(ratio, ratio);
 
-                    const resizeCanvas = () => {
-                        const ratio = Math.max(window.devicePixelRatio || 1, 1);
-                        // simpan data lama sebelum resize
-                        let data = this.signaturePadInstance ? this.signaturePadInstance.toData() : null;
-
-                        canvas.width = canvas.offsetWidth * ratio;
-                        canvas.height = 250 * ratio;
-                        canvas.getContext("2d").scale(ratio, ratio);
-
-                        if (this.signaturePadInstance) {
-                            this.signaturePadInstance.clear();
-                            if (data) {
-                                this.signaturePadInstance.fromData(data);
-                            }
-                        }
-                    };
-
-                    this.signaturePadInstance = new SignaturePad(canvas, {
-                        backgroundColor: 'rgba(255,255,255,0)',
-                    });
+                    this.signaturePadInstance = new SignaturePad(canvas);
 
                     if (this.state) {
                         this.signaturePadInstance.fromDataURL(this.state);
@@ -51,11 +35,6 @@
                     this.signaturePadInstance.addEventListener("endStroke", () => {
                         this.state = this.signaturePadInstance.toDataURL();
                     });
-
-                    // panggil sekali waktu awal
-                    resizeCanvas();
-                    // update kalau ukuran layar berubah
-                    window.addEventListener("resize", resizeCanvas);
                 });
             },
 
