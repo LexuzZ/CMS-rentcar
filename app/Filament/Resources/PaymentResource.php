32417180ddd289f349641e44e0a5cf3772182f37
Haerul Taka <haerultaka255@gmail.com>
2025-09-19 19:39:55 +0800

    update edit form invoice

--- app/Filament/Resources/PaymentResource.php
+++ app/Filament/Resources/PaymentResource.php
@@ -261,7 +261,7 @@ class PaymentResource extends Resource
                                     ->label('Total Denda')
                                     ->state(fn($record) => $record->invoice->booking?->penalty->sum('amount') ?? 0)
                                     ->formatStateUsing(fn($state) => 'Rp ' . number_format($state, 0, ',', '.')),
-                                \Filament\Infolists\Components\TextEntry::make('sisa_pembayaran')
+                                \Filament\Infolists\Components\TextEntry::make('total_bayar')
                                     ->label('Sisa Pembayaran')
                                     ->state(function ($record) {
                                         $biayaSewa = $record->invoice->booking?->estimasi_biaya ?? 0;
