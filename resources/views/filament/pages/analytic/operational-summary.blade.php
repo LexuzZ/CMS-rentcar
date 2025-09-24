afe56ef1b49e22fe6df9af6dbe21f2e9606ec5e5
Haerul Taka <haerultaka255@gmail.com>
2025-09-24 09:55:07 +0800

    add filter di ringkasan operasional

--- resources/views/filament/pages/analytic/operational-summary.blade.php
+++ resources/views/filament/pages/analytic/operational-summary.blade.php
@@ -37,7 +37,49 @@
                                     @if (!is_null($row['change']))
                                         @php
                                             $isPositive = $row['change'] >= 0;
-                                            $changeColor = $isPositive ? 'text-warning-600' : 'text-danger-600';
+                                            $changeColor = $isPositive ? 'text-success-600' : 'text-danger-600';
+                                            $icon = $isPositive ? 'heroicon-m-arrow-trending-up' : 'heroicon-m-arrow-trending-down';
+                                        @endphp
+                                        <span class="{{ $changeColor }} flex items-center justify-center gap-1">
+                                            <x-filament::icon :icon="$icon" class="w-4 h-4" />
+                                            {{ number_format($row['change'], 1) }}%
+                                        </span>
+                                    @else
+                                        <span class="text-gray-400">-</span>
+                                    @endif
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="overflow-x-auto">
+                <table class="w-full text-sm text-left text-gray-600 dark:text-gray-300 border border-gray-200 dark:border-gray-700 rounded-lg">
+                    <thead class="bg-gray-100 dark:bg-gray-800">
+                        <tr>
+                            <th class="px-4 py-2 text-left">Kategori</th>
+                            <th class="px-4 py-2 text-right">Nilai</th>
+                            <th class="px-4 py-2 text-center">Perubahan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($summaryTableData as $row)
+                            <tr class="border-b dark:border-gray-700">
+                                <td class="px-4 py-2 font-medium text-gray-900 dark:text-white">
+                                    {{ $row['label'] }}
+                                </td>
+                                <td class="px-4 py-2 text-right">
+                                    @if (is_numeric($row['value']))
+                                        Rp {{ number_format($row['value'], 0, ',', '.') }}
+                                    @else
+                                        {{ $row['value'] }}
+                                    @endif
+                                </td>
+                                <td class="px-4 py-2 text-center">
+                                    @if (!is_null($row['change']))
+                                        @php
+                                            $isPositive = $row['change'] >= 0;
+                                            $changeColor = $isPositive ? 'text-success-600' : 'text-danger-600';
                                             $icon = $isPositive ? 'heroicon-m-arrow-trending-up' : 'heroicon-m-arrow-trending-down';
                                         @endphp
                                         <span class="{{ $changeColor }} flex items-center justify-center gap-1">
