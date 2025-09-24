e30c609701d6f2b890a5eff1fb190399b69e0a87
Haerul Taka <haerultaka255@gmail.com>
2025-09-24 09:43:31 +0800

    add filter di ringkasan operasional

--- resources/views/filament/pages/analytic/operational-summary.blade.php
+++ resources/views/filament/pages/analytic/operational-summary.blade.php
@@ -37,7 +37,7 @@
                                     @if (!is_null($row['change']))
                                         @php
                                             $isPositive = $row['change'] >= 0;
-                                            $changeColor = $isPositive ? 'text-success-700' : 'text-danger-600';
+                                            $changeColor = $isPositive ? 'text-success-600' : 'text-danger-600';
                                             $icon = $isPositive ? 'heroicon-m-arrow-trending-up' : 'heroicon-m-arrow-trending-down';
                                         @endphp
                                         <span class="{{ $changeColor }} flex items-center justify-center gap-1">
@@ -45,7 +45,7 @@
                                             {{ number_format($row['change'], 1) }}%
                                         </span>
                                     @else
-                                        <span class="text-gray-400">-</span>
+                                        <span class="text-success-600">-</span>
                                     @endif
                                 </td>
                             </tr>
