149703c1dd8d12506dcabfcb8645db9e2006dd8c
Haerul Taka <haerultaka255@gmail.com>
2025-09-24 09:32:22 +0800

    add tabel ringkasan

--- resources/views/filament/pages/analytic/operational-summary.blade.php
+++ resources/views/filament/pages/analytic/operational-summary.blade.php
@@ -28,7 +28,7 @@
                             </td>
                             <td class="px-4 py-3 text-right">
                                 @if ($row['change'] !== null)
-                                    <span class="{{ $row['change'] >= 0 ? 'text-success' : 'text-danger' }}">
+                                    <span class="{{ $row['change'] >= 0 ? 'text-success-600' : 'text-danger-600' }}">
                                         {{ number_format($row['change'], 1) }}%
                                     </span>
                                 @else
