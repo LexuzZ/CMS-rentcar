3bd83ed5099ff42b47d7d97c8f1f7e10c2fe6ccd
Haerul Taka <haerultaka255@gmail.com>
2025-09-24 11:57:55 +0800

    add filter di ringkasan operasional

--- app/Filament/Pages/Analytic/OperationalSummary.php
+++ app/Filament/Pages/Analytic/OperationalSummary.php
@@ -203,9 +203,9 @@ class OperationalSummary extends Page implements HasForms
 
         ];
         $this->summaryTableData = [
-            ['label' => 'Pendapatan Kotor', 'value' => $RevenueMonth, 'change' => $RevenueChange],
+            ['label' => 'Pendapatan ', 'value' => $RevenueMonth, 'change' => $RevenueChange],
             ['label' => 'Pendapatan Pokok', 'value' => $pokokThisMonth, 'change' => $pokokChange],
-            ['label' => 'Piutang Kotor', 'value' => $receivablesThisMonth, 'change' => $receivablesChange],
+            ['label' => 'Piutang ', 'value' => $receivablesThisMonth, 'change' => $receivablesChange],
             ['label' => 'Profit Garasi', 'value' => $incomeThisMonth, 'change' => $incomeChange],
             ['label' => 'Total Pengeluaran', 'value' => $expenseThisMonth, 'change' => $expenseChange],
             ['label' => 'Laba Bersih', 'value' => $profitThisMonth, 'change' => $profitChange],
