8d2a2c626f5330d39d36886a4313689d877cc02e
Haerul Taka <haerultaka255@gmail.com>
2025-09-30 10:30:34 +0800

    update laba bersih stat keuangan

--- app/Filament/Resources/BookingResource.php
+++ app/Filament/Resources/BookingResource.php
@@ -393,9 +393,9 @@ class BookingResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('status')
                     ->badge()->alignCenter()
-                    ->colors(['success' => 'aktif', 'info' => 'booking', 'gray' => 'selesai', 'danger' => 'batal'])
+                    ->colors(['success' => 'disewa', 'info' => 'booking', 'gray' => 'selesai', 'danger' => 'batal'])
                     ->formatStateUsing(fn($state) => match ($state) {
-                        'aktif' => 'Aktif',
+                        'disewa' => 'Disewa',
                         'booking' => 'Booking',
                         'selesai' => 'Selesai',
                         'batal' => 'Batal',
