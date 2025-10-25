17857dcab68e06856457c9619e718276a3b7c533
Haerul Taka <haerultaka255@gmail.com>
2025-10-25 18:41:01 +0800

    mobile responsive

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Booking Kendaraan</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/css/tom-select.css" rel="stylesheet">
