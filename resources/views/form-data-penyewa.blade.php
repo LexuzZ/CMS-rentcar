6efe678b5ca9cb3d444cebf39599a2d368f06a20
Haerul Taka <haerultaka255@gmail.com>
2025-10-25 18:43:39 +0800

    mobile responsive

--- resources/views/form-data-penyewa.blade.php
+++ resources/views/form-data-penyewa.blade.php
@@ -2,7 +2,8 @@
 <html lang="id">
 
 <head>
-    <meta charset="UTF-8" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Penyewa</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
