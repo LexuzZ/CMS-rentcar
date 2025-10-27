16a79f2a3723c6e112c0391f48c15c690285a0a3
Haerul Taka <haerultaka255@gmail.com>
2025-10-27 08:17:07 +0800

    update grid dashboard

--- resources/views/filament/widgets/mobil-kembali-card.blade.php
+++ resources/views/filament/widgets/mobil-kembali-card.blade.php
@@ -3,7 +3,7 @@
     @if ($bookingsToday->isNotEmpty())
         <div class="mb-6">
             <h3 class="text-lg font-semibold mb-4 pt-4">Mobil Kembali Hari Ini</h3>
-            <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 @foreach ($bookingsToday as $record)
                     {{-- Mengirimkan tema 'danger' untuk kartu hari ini --}}
                     @include('filament.widgets.components.mobil-kembali-booking-card', ['record' => $record, 'theme' => 'danger'])
