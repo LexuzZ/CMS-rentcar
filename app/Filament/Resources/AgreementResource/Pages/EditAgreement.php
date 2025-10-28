2f622e017edf9b6bdcffd38ca9e98fc802d12982
Haerul Taka <haerultaka255@gmail.com>
2025-10-28 17:59:21 +0800

    update component form keluar

--- app/Filament/Resources/AgreementResource/Pages/EditAgreement.php
+++ app/Filament/Resources/AgreementResource/Pages/EditAgreement.php
@@ -6,10 +6,61 @@ use App\Filament\Resources\AgreementResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Carbon\Carbon;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 
 class EditAgreement extends EditRecord
 {
     protected static string $resource = AgreementResource::class;
+    protected function getHeaderInfolist(): ?Infolist
+    {
+        return Infolist::make()
+            ->schema([
+                Section::make('Detail Booking')
+                    ->schema([
+                        TextEntry::make('id')->label('Booking ID'),
+                        TextEntry::make('customer.nama')->label('Nama Customer'),
+                        TextEntry::make('car.carModel.name')->label('Nama Mobil'),
+
+                        TextEntry::make('tanggal_keluar')
+                            ->label('Tanggal Keluar')
+                            ->formatStateUsing(fn($state) => $state ? Carbon::parse($state)->format('d M Y') : '-'),
+
+                        TextEntry::make('tanggal_kembali')
+                            ->label('Tanggal Kembali')
+                            ->formatStateUsing(fn($state) => $state ? Carbon::parse($state)->format('d M Y') : '-'),
+
+                        TextEntry::make('car.nopol')->label('No. Polisi'),
+
+                        TextEntry::make('waktu_keluar')
+                            ->label('Waktu Keluar')
+                            ->formatStateUsing(fn($state) => $state ? Carbon::parse($state)->format('H:i') : '-'),
+
+                        TextEntry::make('waktu_kembali')
+                            ->label('Waktu Kembali')
+                            ->formatStateUsing(fn($state) => $state ? Carbon::parse($state)->format('H:i') : '-'),
+
+                        TextEntry::make('total_hari')
+                            ->label('Total Hari')
+                            ->formatStateUsing(fn($state) => $state ? "{$state} Hari" : '-'),
+
+                        TextEntry::make('invoice.dp')
+                            ->label('Uang Muka (DP)')
+                            ->formatStateUsing(fn($state) => $state ? 'Rp ' . number_format($state, 0, ',', '.') : '-'),
+
+                        TextEntry::make('invoice.sisa_pembayaran')
+                            ->label('Sisa Pembayaran')
+                            ->formatStateUsing(fn($state) => $state ? 'Rp ' . number_format($state, 0, ',', '.') : '-'),
+
+                        TextEntry::make('invoice.total')
+                            ->label('Total Tagihan')
+                            ->formatStateUsing(fn($state) => $state ? 'Rp ' . number_format($state, 0, ',', '.') : '-'),
+                    ])
+                    ->columns(3),
+            ]);
+    }
 
     protected function getHeaderActions(): array
     {
