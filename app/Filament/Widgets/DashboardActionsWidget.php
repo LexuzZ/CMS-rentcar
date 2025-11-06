018cb1a809c1064c3baa3f88a8cff9abb0584348
Haerul Taka <haerultaka255@gmail.com>
2025-11-07 00:31:02 +0800

    add button akses cepa

--- app/Filament/Widgets/DashboardActionsWidget.php
+++ app/Filament/Widgets/DashboardActionsWidget.php
@@ -7,5 +7,5 @@ use Filament\Widgets\Widget;
 class DashboardActionsWidget extends Widget
 {
     protected static string $view = 'filament.widgets.dashboard-actions-widget';
-    protected int | string | array $columnSpan = 'half';
+    protected int | string | array $columnSpan = 'full';
 }
