c02d08e9d5fc9f3c7833f9d1c8a2501c5ae600c5
Haerul Taka <haerultaka255@gmail.com>
2025-11-07 01:04:05 +0800

    add button akses cepa

--- app/Filament/Widgets/DashboardActionsWidget.php
+++ app/Filament/Widgets/DashboardActionsWidget.php
@@ -7,6 +7,6 @@ use Filament\Widgets\Widget;
 class DashboardActionsWidget extends Widget
 {
     protected static string $view = 'filament.widgets.dashboard-actions-widget';
-    protected int | string | array $columnSpan = 'half';
+    protected int | string | array $columnSpan = 'full';
     protected static ?string $heading = 'Akses Cepat';
 }
