5622bc1c19d620a2a54af2c0bd168e7bba189c8b
Haerul Taka <haerultaka255@gmail.com>
2025-11-10 12:38:49 +0800

    edit dashboard

--- resources/views/filament/widgets/dashboard-actions-widget.blade.php
+++ resources/views/filament/widgets/dashboard-actions-widget.blade.php
@@ -3,14 +3,12 @@
         <x-slot name="heading">
             🚀 Quick Menu
         </x-slot>
-        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
-
+        <div class="flex flex-wrap gap-2">
             {{-- TOMBOL 1: Form Sewa --}}
             <a href="{{ \App\Filament\Resources\BookingResource::getUrl('index') }}"
-                class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-blue-100 text-blue-800 hover:bg-blue-200 transition-colors">
+                class="min-w-[calc(50%-0.5rem)] flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-blue-100 text-blue-800 hover:bg-blue-200 transition-colors">
 
-                <div
-                    class="w-8 h-8 rounded-full bg-blue-100 text-blue-800 hover:bg-blue-200 flex items-center justify-center">
+                <div class="w-8 h-8 rounded-full bg-blue-100 text-blue-800 hover:bg-blue-200 flex items-center justify-center">
                     <div class="text-4xl">🧾</div>
                 </div>
 
@@ -21,7 +19,7 @@
 
             {{-- TOMBOL 2: Transaksi --}}
             <a href="{{ \App\Filament\Resources\PaymentResource::getUrl('index') }}"
-                class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm hover:bg-gray-50 dark:text-black dark:hover:text-gray-900 transition-colors">
+                class="min-w-[calc(50%-0.5rem)] flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm hover:bg-gray-50 dark:text-black dark:hover:text-gray-900 transition-colors">
 
                 <div class="w-8 h-8 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
                     <div class="text-4xl">💳</div>
@@ -34,7 +32,7 @@
 
             {{-- TOMBOL 3: Kalender Unit --}}
             <a href="{{ \App\Filament\Pages\VehicleSchedule::getUrl() }}"
-                class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
+                class="min-w-[calc(50%-0.5rem)] flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
 
                 <div class="w-8 h-8 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
                     <div class="text-4xl">📅</div>
@@ -47,7 +45,7 @@
 
             {{-- TOMBOL 4: Checklist Keluar --}}
             <a href="{{ \App\Filament\Resources\AgreementResource::getUrl('index') }}"
-                class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
+                class="min-w-[calc(50%-0.5rem)] flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
 
                 <div class="w-8 h-8 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
                     <div class="text-4xl">✅</div>
@@ -60,7 +58,7 @@
 
             {{-- TOMBOL 5: Checklist Kembali --}}
             <a href="{{ \App\Filament\Resources\ReturnAgreementResource::getUrl('index') }}"
-                class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
+                class="min-w-[calc(50%-0.5rem)] flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
 
                 <div class="w-8 h-8 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
                     <div class="text-6xl">📦</div>
@@ -70,7 +68,6 @@
                     Checklist Kembali
                 </span>
             </a>
-
         </div>
     </x-filament::section>
 </x-filament::widget>
