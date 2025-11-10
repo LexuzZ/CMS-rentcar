75fbdba03c4c0f3dcfdfa4cfaccafe9671e9f50e
Haerul Taka <haerultaka255@gmail.com>
2025-11-10 11:52:11 +0800

    edit dashboard

--- app/Filament/Widgets/DashboardActionsWidget.php
+++ app/Filament/Widgets/DashboardActionsWidget.php
@@ -7,7 +7,7 @@ use Filament\Widgets\Widget;
 class DashboardActionsWidget extends Widget
 {
     protected static string $view = 'filament.widgets.dashboard-actions-widget';
-    protected int | string | array $columnSpan = 'full';
+    protected int|string|array $columnSpan = 10;
     protected static ?int $sort = 4;
     protected static ?string $heading = 'Akses Cepat';
 }
