9c3ee070d0385c9f58d1cfeeb46d525ff4cc0712
Haerul Taka <haerultaka255@gmail.com>
2025-11-10 12:32:47 +0800

    edit dashboard

--- resources/views/filament/widgets/dashboard-actions-widget.blade.php
+++ resources/views/filament/widgets/dashboard-actions-widget.blade.php
@@ -16,59 +16,59 @@
                     <div class="text-4xl">🧾</div>
                 </div>
 
-                <span class="mt-2 text-xs md:text-sm lg:text-lg font-medium text-gray-900 dark:text-gray-100">
+                <span class="mt-1 text-xs md:text-sm lg:text-lg font-medium text-gray-900 dark:text-gray-100">
                     Form Sewa
                 </span>
             </a>
             <a href="{{ \App\Filament\Resources\PaymentResource::getUrl('index') }}"
-                class="flex flex-col items-center justify-center p-2 border rounded-lg shadow-sm  hover:bg-gray-50 dark:text-black dark:hover:text-gray-900 transition-colors">
+                class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm  hover:bg-gray-50 dark:text-black dark:hover:text-gray-900 transition-colors">
 
                 {{-- Ganti ikon di sini --}}
-                <div class="w-16 h-16 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
+                <div class="w-8 h-8 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
                     {{-- IKON (ukurannya lebih kecil dari lingkaran) --}}
                     <div class="text-4xl">💳</div>
                 </div>
 
-                <span class="mt-2 text-xs md:text-sm lg:text-lg font-medium text-gray-900 dark:text-gray-100">
+                <span class="mt-1 text-xs md:text-sm lg:text-lg font-medium text-gray-900 dark:text-gray-100">
                     Transaksi
                 </span>
             </a>
             <a href="{{ \App\Filament\Pages\VehicleSchedule::getUrl() }}"
-                class="flex flex-col items-center justify-center p-2 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
+                class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
 
                 {{-- Ganti ikon di sini --}}
-                <div class="w-16 h-16 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
+                <div class="w-8 h-8 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
                     {{-- IKON (ukurannya lebih kecil dari lingkaran) --}}
                     <div class="text-4xl">📅</div>
                 </div>
 
-                <span class="mt-2 text-xs md:text-sm lg:text-lg font-medium text-gray-900 dark:text-gray-100">
+                <span class="mt-1 text-xs md:text-sm lg:text-lg font-medium text-gray-900 dark:text-gray-100">
                     Kalender Unit
                 </span>
             </a>
             <a href="{{ \App\Filament\Resources\AgreementResource::getUrl('index') }}"
-                class="flex flex-col items-center justify-center p-2 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
+                class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
 
                 {{-- Ganti ikon di sini --}}
-                <div class="w-16 h-16 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
+                <div class="w-8 h-8 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
                     {{-- IKON (ukurannya lebih kecil dari lingkaran) --}}
                     <div class="text-4xl">✅</div>
                 </div>
 
-                <span class="mt-2 text-xs md:text-sm lg:text-lg font-medium text-gray-900 dark:text-gray-100">
+                <span class="mt-1 text-xs md:text-sm lg:text-lg font-medium text-gray-900 dark:text-gray-100">
                     Checklist Keluar
                 </span>
             </a>
             <a href="{{ \App\Filament\Resources\ReturnAgreementResource::getUrl('index') }}"
-                class="flex flex-col items-center justify-center p-2 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
+                class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
 
                 {{-- Ganti ikon di sini --}}
-                <div class="w-16 h-16 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
+                <div class="w-8 h-8 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
                     {{-- IKON (ukurannya lebih kecil dari lingkaran) --}}
                     <div class="text-6xl">📦</div>
                 </div>
 
-                <span class="mt-2 text-xs md:text-sm lg:text-lg font-medium text-gray-900 dark:text-gray-100">
+                <span class="mt-1 text-xs md:text-sm lg:text-lg font-medium text-gray-900 dark:text-gray-100">
                     Checklist Kembali
                 </span>
             </a>
