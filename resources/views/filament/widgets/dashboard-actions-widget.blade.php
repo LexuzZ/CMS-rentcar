9eff5e79c5ae3e8291ce4e79484fad28c6953515
Haerul Taka <haerultaka255@gmail.com>
2025-11-10 12:35:29 +0800

    edit dashboard

--- resources/views/filament/widgets/dashboard-actions-widget.blade.php
+++ resources/views/filament/widgets/dashboard-actions-widget.blade.php
@@ -3,16 +3,14 @@
         <x-slot name="heading">
             🚀 Quick Menu
         </x-slot>
-        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2">
+        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-2">
 
             {{-- TOMBOL 1: Form Sewa --}}
             <a href="{{ \App\Filament\Resources\BookingResource::getUrl('index') }}"
                 class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-blue-100 text-blue-800 hover:bg-blue-200 transition-colors">
 
-                {{-- Ganti ikon di sini --}}
                 <div
                     class="w-8 h-8 rounded-full bg-blue-100 text-blue-800 hover:bg-blue-200 flex items-center justify-center">
-                    {{-- IKON (ukurannya lebih kecil dari lingkaran) --}}
                     <div class="text-4xl">🧾</div>
                 </div>
 
@@ -20,12 +18,12 @@
                     Form Sewa
                 </span>
             </a>
-            <a href="{{ \App\Filament\Resources\PaymentResource::getUrl('index') }}"
-                class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm  hover:bg-gray-50 dark:text-black dark:hover:text-gray-900 transition-colors">
 
-                {{-- Ganti ikon di sini --}}
+            {{-- TOMBOL 2: Transaksi --}}
+            <a href="{{ \App\Filament\Resources\PaymentResource::getUrl('index') }}"
+                class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm hover:bg-gray-50 dark:text-black dark:hover:text-gray-900 transition-colors">
+
                 <div class="w-8 h-8 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
-                    {{-- IKON (ukurannya lebih kecil dari lingkaran) --}}
                     <div class="text-4xl">💳</div>
                 </div>
 
@@ -33,12 +31,12 @@
                     Transaksi
                 </span>
             </a>
+
+            {{-- TOMBOL 3: Kalender Unit --}}
             <a href="{{ \App\Filament\Pages\VehicleSchedule::getUrl() }}"
                 class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
 
-                {{-- Ganti ikon di sini --}}
                 <div class="w-8 h-8 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
-                    {{-- IKON (ukurannya lebih kecil dari lingkaran) --}}
                     <div class="text-4xl">📅</div>
                 </div>
 
@@ -46,12 +44,12 @@
                     Kalender Unit
                 </span>
             </a>
+
+            {{-- TOMBOL 4: Checklist Keluar --}}
             <a href="{{ \App\Filament\Resources\AgreementResource::getUrl('index') }}"
                 class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
 
-                {{-- Ganti ikon di sini --}}
                 <div class="w-8 h-8 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
-                    {{-- IKON (ukurannya lebih kecil dari lingkaran) --}}
                     <div class="text-4xl">✅</div>
                 </div>
 
@@ -59,12 +57,12 @@
                     Checklist Keluar
                 </span>
             </a>
+
+            {{-- TOMBOL 5: Checklist Kembali --}}
             <a href="{{ \App\Filament\Resources\ReturnAgreementResource::getUrl('index') }}"
                 class="flex flex-col items-center justify-center p-1 border rounded-lg shadow-sm bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
 
-                {{-- Ganti ikon di sini --}}
                 <div class="w-8 h-8 rounded-full bg-blue-100 dark:bg-blue-900/50 flex items-center justify-center">
-                    {{-- IKON (ukurannya lebih kecil dari lingkaran) --}}
                     <div class="text-6xl">📦</div>
                 </div>
 
@@ -73,12 +71,6 @@
                 </span>
             </a>
 
-
-
         </div>
-        {{-- <x-filament::card> --}}
-
-
     </x-filament::section>
-
 </x-filament::widget>
