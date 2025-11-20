b379cd8dc4ba0c5d8955f6877a9655df244abc63
Haerul Taka <haerultaka255@gmail.com>
2025-11-20 21:04:11 +0800

    edit fitur activity log

--- app/Filament/Widgets/UserActivityLog.php
+++ app/Filament/Widgets/UserActivityLog.php
@@ -11,7 +11,8 @@ class UserActivityLog extends BaseWidget
 {
     protected static ?int $sort = 6;
 
-    protected int|string|array $columnSpan = 'full';
+    // protected int|string|array $columnSpan = 'full';
+    protected int | string | array $columnSpan = 'full';
 
     public function table(Table $table): Table
     {
