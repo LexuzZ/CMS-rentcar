fde527cf31c6cca8635be844148c474274bb0b4e
Haerul Taka <haerultaka255@gmail.com>
2025-11-21 10:01:27 +0800

    edit mobil keluar dan kembali card

--- resources/views/filament/widgets/components/booking-card.blade.php
+++ resources/views/filament/widgets/components/booking-card.blade.php
@@ -71,7 +71,7 @@
         <div class="flex justify-between">
             <span class="text-gray-500 dark:text-gray-400 text-xs">Staff</span>
             <span class=" bg-green-500 text-gray-900 dark:text-white text-xs">
-                {{ $record->driver_pengantaran_id->nama ?? 'N/A' }}
+                {{ $record->driverPengantaran->nama ?? 'N/A' }}
             </span>
         </div>
 
