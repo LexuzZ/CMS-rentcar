c42dad22f92a1a6fd56e3e5e649083468707308b
Haerul Taka <haerultaka255@gmail.com>
2025-12-27 16:44:57 +0800

    ubah redirect inv

--- app/Filament/Resources/InvoiceResource/Pages/EditInvoice.php
+++ app/Filament/Resources/InvoiceResource/Pages/EditInvoice.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\InvoiceResource\Pages;
 
+use App\Filament\Resources\BookingResource;
 use App\Filament\Resources\InvoiceResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
@@ -16,5 +17,13 @@ class EditInvoice extends EditRecord
         return [
             Actions\DeleteAction::make()->label('Hapus Faktur')->color('danger'),
         ];
+    }
+    protected function getRedirectUrl(): string
+    {
+        // Ambil data invoice yang baru saja dibuat
+        $invoice = $this->getRecord();
+
+        // Arahkan kembali ke halaman 'view' dari booking yang berelasi
+        return BookingResource::getUrl('view', ['record' => $invoice->id]);
     }
 }
