90805d69f38eb53e091660616e9b1c018a2a841b
Haerul Taka <haerultaka255@gmail.com>
2025-12-30 13:55:54 +0800

    update ketersediaan mobil

--- app/Filament/Pages/Worksheet/Widgets/AvailableCarsOverview.php
+++ app/Filament/Pages/Worksheet/Widgets/AvailableCarsOverview.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Pages\Worksheet\Widgets;
 
 use App\Models\Car;
+use Carbon\Carbon;
 use Filament\Widgets\Widget;
 use Illuminate\Support\Facades\DB;
 
@@ -12,19 +13,32 @@ class AvailableCarsOverview extends Widget
     protected int|string|array $columnSpan = 'full';
 
     public function getViewData(): array
-    {
-         $availableCars = Car::with(['carModel']) // <-- Eager load relasi
-        ->where('status', 'ready')
+{
+    $today = Carbon::today();
+    $tomorrow = Carbon::tomorrow();
+
+    $availableCars = Car::with(['carModel', 'bookings'])
         ->where('garasi', 'SPT')
+        ->where(function ($query) use ($today) {
+            $query
+                // Mobil tanpa booking aktif
+                ->whereDoesntHave('bookings', function ($q) use ($today) {
+                    $q->whereDate('tanggal_keluar', '<=', $today);
+                })
+                // ATAU booking tapi keluarnya BESOK ke atas
+                ->orWhereHas('bookings', function ($q) use ($today) {
+                    $q->whereDate('tanggal_keluar', '>', $today);
+                });
+        })
         ->get();
 
-    // Grouping sekarang dilakukan berdasarkan nama merek dari relasi
     $groupedCars = $availableCars->groupBy('carModel.name');
 
     return [
         'cars' => $groupedCars,
     ];
-    }
+}
+
 }
 
 
