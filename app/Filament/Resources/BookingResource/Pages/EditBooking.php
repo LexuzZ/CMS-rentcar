2a5b08165c66d14eb716af5465d7afb3b6f3f665
Haerul Taka <haerultaka255@gmail.com>
2026-01-13 21:21:02 +0800

    fix 503 error

--- app/Filament/Resources/BookingResource/Pages/EditBooking.php
+++ app/Filament/Resources/BookingResource/Pages/EditBooking.php
@@ -39,19 +39,6 @@ class EditBooking extends EditRecord
     }
     protected function getRedirectUrl(): string
     {
-        // Ambil booking_id dari record
-        $invoice = $this->record->invoice;
-
-        if ($invoice) {
-            // Redirect ke halaman View Invoice di Filament
-            return \App\Filament\Resources\InvoiceResource::getUrl('view', [
-                'record' => $invoice->id,
-            ]);
-        }
-
-        // Kalau booking belum ada invoice, fallback ke index booking
-        return BookingResource::getUrl('view', [
-            'record' => $this->record->id,
-        ]);
+        return $this->getResource()::getUrl('index');
     }
 }
