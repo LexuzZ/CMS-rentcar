d09b6c33140b8883c0e5d0fb790af0d71284331d
Haerul Taka <haerultaka255@gmail.com>
2026-01-14 16:00:37 +0800

    update invoice

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -19,6 +19,10 @@ class Invoice extends Model
         'status',
         'tanggal_invoice',
     ];
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
+    }
     public function payments(): HasMany
     {
         return $this->hasMany(Payment::class);
