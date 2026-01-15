ecf106fb936f76d0c7cc94bdc96154ac5b8c6abd
Haerul Taka <haerultaka255@gmail.com>
2026-01-15 10:42:07 +0800

    update status inv

--- app/Filament/Resources/PaymentResource/Pages/ListPayments.php
+++ app/Filament/Resources/PaymentResource/Pages/ListPayments.php
@@ -10,13 +10,13 @@ class ListPayments extends ListRecords
 {
     protected static string $resource = PaymentResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make()->label('Pembayaran') // ubah teks tombol
-                // ->label('Tambah Mobil')
-                ->icon('heroicon-o-plus')
-                ->color('success')
-        ];
-    }
+    // protected function getHeaderActions(): array
+    // {
+    //     return [
+    //         Actions\CreateAction::make()->label('Pembayaran') // ubah teks tombol
+    //             // ->label('Tambah Mobil')
+    //             ->icon('heroicon-o-plus')
+    //             ->color('success')
+    //     ];
+    // }
 }
