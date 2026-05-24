f0696fd117db3c32d0f854e1f09123efb0e1bdee
Haerul Taka <haerultaka255@gmail.com>
2026-05-24 17:59:42 +0800

    ubah penamaan

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -60,6 +60,7 @@ class AdminPanelProvider extends PanelProvider
             // ->brandLogo(asset('public/spt.png'))
             ->font('Poppins')
             ->databaseNotifications()
+            ->viteTheme('resources/css/filament/admin/theme.css')
             // ->spa()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
