e487029b6d3f0bc402c720d1da4980c763262756
Haerul Taka <haerultaka255@gmail.com>
2026-06-01 21:11:31 +0800

    edit piutang

--- database/migrations/2026_06_01_200907_add_last_login_columns_to_users_table.php
+++ database/migrations/2026_06_01_200907_add_last_login_columns_to_users_table.php
@@ -12,6 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->timestamp('last_login_at')->nullable();
+            $table->string('last_login_ip')->nullable();
             //
         });
     }
@@ -22,7 +24,10 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn([
+                'last_login_at',
+                'last_login_ip',
+            ]);
         });
     }
 };
