931198bc60ad7449916a9f57bfb11ee5c163dbd0
Haerul Taka <haerultaka255@gmail.com>
2026-07-04 17:08:40 +0800

    move file avaliable car

--- app/Filament/Widgets/RecentTransactions.php
+++ app/Filament/Widgets/RecentTransactions.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Builder;
 class RecentTransactions extends BaseWidget
 {
     protected static ?string $heading = 'Transaksi Hari Ini';
-    protected static ?int $sort = 2;
+    protected static ?int $sort = 3;
 
     protected static bool $isLazy = true;
     protected static ?int $pollingInterval = null;
