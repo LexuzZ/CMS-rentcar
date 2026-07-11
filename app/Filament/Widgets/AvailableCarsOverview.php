8048049dd9747c627234ca55a5b143868756318c
Haerul Taka <haerultaka255@gmail.com>
2026-07-11 21:56:45 +0800

    tata letak dashboadrd

--- app/Filament/Widgets/AvailableCarsOverview.php
+++ app/Filament/Widgets/AvailableCarsOverview.php
@@ -9,7 +9,11 @@ use Illuminate\Support\Facades\DB;
 class AvailableCarsOverview extends Widget
 {
     protected static string $view = 'filament.widgets.available-cars-overview';
-    protected int|string|array $columnSpan = 'full';
+    protected int|string|array $columnSpan = [
+        'sm' => 'full',
+        'md' => '10',
+        'lg' => '10',
+    ];
     protected static ?int $sort = 2;
 
     public function getViewData(): array
